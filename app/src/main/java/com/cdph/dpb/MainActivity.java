5d35deb4cb7113ab38ebe78f1bc0faf2d8cb124e
SnoopyCodeX <extremeclasherph@gmail.com>
2020-04-19 20:59:15 +0800

    Initial commit

---
+++ app/src/main/java/com/cdph/dpb/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cdph.dpb;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
